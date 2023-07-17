fce1af486ff5f00309fed3bc80b521c7432497aa
Manasvini Narayanan <86313692+mana-nara@users.noreply.github.com>
2023-07-17 15:12:31 +0530

    Created using Colaboratory

---
+++ stockmarketprediction.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyP76Q9FgS1DOmTFKHfsmzOe",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mana-nara/stock-market-predication/blob/main/stockmarketprediction.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "uDgTL8-WOKsi"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
